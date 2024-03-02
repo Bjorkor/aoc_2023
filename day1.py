52f0d12c1f5054a73f47a7f108d0e8e11a35b3a9
Bjorkor <barkdawg11@gmail.com>
2024-03-02 13:08:14 -0600

    idk

--- day1.py
+++ day1.py
@@ -1,7 +1,27 @@
 import pandas as pd
+import re
 from icecream import ic
 
-
+def words_2_int(row):
+    new_row = None
+    sub_dict = {
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9',
+    }
+    for item in sub_dict:
+        if new_row is None:
+            new_row = re.sub(item, sub_dict[item], row)
+        else:
+            new_row = re.sub(item, sub_dict[item], new_row)
+    ic(new_row)
+    return new_row
 def get_cb_value(row):
     char_list = []
     for char in row:
@@ -21,6 +41,8 @@ with open('day1input.txt') as file:
 
 split_content = content.split('\n')
 content_series = pd.Series(split_content)
-answer_series = content_series.apply(get_cb_value)
+words_series = content_series.apply(words_2_int)
+answer_series = words_series.apply(get_cb_value)
 final_cb = answer_series.sum() 
 print(final_cb)
+
